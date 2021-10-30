42896c8499e26afe6124f820905af51699076ed9
AkhatKhusainov <91784916+AkhatKhusainov@users.noreply.github.com>
2021-10-31 01:07:22 +0300

    Update ex1-3.cpp

--- ex1-3.cpp
+++ ex1-3.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+using namespace std;
+void print_array(int N, int a[]) {
+	for (int i = 1; i <= N; i++) {
+		cout << a[i]<< ' ';
+	}
 
+}
+void ex1() {
+	int N;
+	int D;
+	cin >> N;
+	cin >> D;
+	int a[20];
+	for (int i = N; i <= 20; i++) {
+		a[i] = i + D;
+	}
+	print_array(20, a);
+}
+int main(){
+	ex1();
+}
